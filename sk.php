a6a0518df137576bfe11b94e34e566ff6377fac4
Tehman Sardar <tehmansardar@hotmail.com>
2021-09-22 02:24:17 +0500

    Initializing CPT

--- sk.php
+++ sk.php
@@ -13,13 +13,49 @@ Text Domain: Sk-Plugin
 
 // ways of restricting
 
-if (!defined('ABSPATH')) {
-    die;
-}
 
 defined('ABSPATH') or die('Hey, you can\t access this file, you silly human');
 
-if (!function_exists('add_action')) {
-    echo 'Hey, you can\t access this file, you silly human';
-    exit;
+class SK
+{
+
+    function __construct()
+    {
+        add_action('init', [$this, 'custom_post_type']);
+    }
+
+    function activate()
+    {
+        // Generate CPT
+        $this->custom_post_type();
+        // Flush rewrtie rules
+        flush_rewrite_rules();
+    }
+
+    function deactivate()
+    {
+        // Flush rewrite rules
+        flush_rewrite_rules();
+    }
+
+    function uninstall()
+    {
+        // delete CPT
+        // delete all the plugin data from DB
+    }
+
+    function custom_post_type()
+    {
+        register_post_type('book', ['public' => true, 'label' => 'Books']);
+    }
 }
+
+if (class_exists('SK')) {
+    $sk = new SK();
+}
+
+// activation
+register_activation_hook(__FILE__, [$sk, 'activate']);
+
+// deactivation
+register_deactivation_hook(__FILE__, [$sk, 'deactivate']);
