df45641ffebba34a6068b4fb6896cc407b569543
Tehman Sardar <tehmansardar@hotmail.com>
2021-09-22 02:07:21 +0500

    Ways of restricting

--- sk.php
+++ sk.php
@@ -10,3 +10,16 @@ Description: Dynamic CPT, Widgets, Taxonomies, Widgets
 License: GPLv2 or later
 Text Domain: Sk-Plugin
 */
+
+// ways of restricting
+
+if (!defined('ABSPATH')) {
+    die;
+}
+
+defined('ABSPATH') or die('Hey, you can\t access this file, you silly human');
+
+if (!function_exists('add_action')) {
+    echo 'Hey, you can\t access this file, you silly human';
+    exit;
+}
