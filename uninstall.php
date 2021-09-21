3c75f9550435f722b3fb86ab512706f68b2074b8
Tehman Sardar <tehmansardar@hotmail.com>
2021-09-22 02:45:25 +0500

    Accessing database to on uninstall plugin

---
+++ uninstall.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Trigger this file on Plugin uninstall
+ * 
+ * @package Sk
+ */
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+// Clear Databse Storage
+$books = get_post(['post_type' => 'book', 'numberposts' => -1]);
+
+foreach ($books as $book) {
+    wp_delete_post($book->ID, true);
+}
+
+// Access the database via SQl
+global $wpdb;
+$wpdb->query('DELETE FROM wp_posts WHERE post_type='book');
+$wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
+$wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)");
