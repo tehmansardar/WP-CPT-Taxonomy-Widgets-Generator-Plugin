7877c698001e1e199520bd2c3bceeb593cfd4f79
Tehman Sardar <tehmansardar@hotmail.com>
2021-09-22 05:12:17 +0500

    composer initialization

--- sk.php
+++ sk.php
@@ -29,6 +29,16 @@ class SK
     {
         add_action('admin_enqueue_scripts', [$this, 'enqueue']);
         add_action('admin_menu', [$this, 'add_admin_pages']);
+        add_filter("plugin_action_links_$this->plugin", [$this, 'settings_link']);
+    }
+
+    public function settings_link($links)
+    {
+
+        $settings_link = '<a href="admin.php?page=sk_plugin">Settings</a>';
+        array_push($links, $settings_link);
+
+        return $links;
     }
 
     public function add_admin_pages()
